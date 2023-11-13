d055a3d8ca58c02c102b1ed67cc4aaa4ec6c0710
MariamJel <112167387+MariamJel@users.noreply.github.com>
2023-11-13 13:01:22 +0400

    Create Um.py

---
+++ 31.Regular, Um, Expressions/Um.py
@@ -0,0 +1,17 @@
+import re
+import sys
+
+
+def main():
+    print(count(input("Text: ")))
+
+
+def count(s):
+    ums = re.findall(r"\b\W*um\W*\b", s, re.IGNORECASE)
+    return len(ums)
+
+
+
+
+if __name__ == "__main__":
+    main()
